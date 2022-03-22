e0f9b7bc80de91b183413eb9a15a4cd83a5b19c0
Ricardo Rios <rickrios23@hotmail.com>
2022-03-22 15:34:50 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import {GetServerSideProps} from 'next';
+import {GetStaticProps} from 'next';
 import Head from "next/head";
 
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from '../services/stripe';
 
 import styles from "./home.module.scss"
+
+//Ways of making a call
+//Client-side
+//Server-side
+//Static Site Generation
 
 interface HomeProps {
   product: {
@@ -38,7 +43,7 @@ export default function Home({  product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Kg8NyLnpQLVWxTxJ8YhejEd')
 
   const product = {
@@ -51,6 +56,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
